fc3d16596a9433a212d4d66d5b3a0517fcaf7a3e
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-10 12:56:38 +0700

    jika rear berada di posisi akhir maka kembali ke array awal

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -33,3 +33,11 @@ class Queues {
              REAR = 0;
         }
         else {
+            // jika REAR berada di posisi terakhir array, kembali ke awal array 
+            if (REAR == max -1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }  
+        queues_array [REAR] = num;
+    }
