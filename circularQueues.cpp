d9d32708736119d8299a9244eeb7abf84e833740
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-10 12:59:48 +0700

    menampilkan elemen dari front ke array lalu ke awal rear

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -41,3 +41,65 @@ class Queues {
         }  
         queues_array [REAR] = num;
     }
+
+    void remove() {
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n"
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queues_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max -1)
+            FRONT = 0;
+        else
+            FRONT = FRONT + 1;
+        
+        }
+    }
+
+    void display() {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;;
+
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empaty\n";
+            return;
+        }
+        cout << "\nElements in the queue are...\n";
+
+        // jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queues_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        else {
+            // jika FRONT_position > REAR_position, iterasi dari FRONT  hingga akhir array
+            while (FRONT_position <= max -1) {
+                cout << queues_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queues_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        
+    }
+};
+
