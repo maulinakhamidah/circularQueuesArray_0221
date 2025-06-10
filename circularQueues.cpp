8ea1f354c1b2dfeb39968c5a56fe6d5cd8a9db2d
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-10 12:55:25 +0700

    mengecek apakah antrian kosong

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -26,3 +26,10 @@ class Queues {
             cout << "\nQueue overflow\n";
             return;
         }
+
+        // cek apakah antrian kosong 
+        if (FRONT == max -1) {
+             FRONT = 0;
+             REAR = 0;
+        }
+        else {
