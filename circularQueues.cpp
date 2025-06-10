031424630204d4480e399e94804ec4110a4986ca
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-10 13:03:56 +0700

    untuk perulangan

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -108,4 +108,17 @@ int main()
     Queues q;
     char ch;
 
-    
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values"  << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choise (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+           
